251f7a52b05ace4ff64e0887d0a810452d2523f0
Hari Laxmi <harinarayana.laxmi@gmail.com>
2022-01-23 12:16:47 +0530

    section 6

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController <ApplicationController
 before_action:set_article, only: [:show,:edit,:update,:destroy]
+before_action :require_user, except: [:show, :index]
+before_action :require_same_user, only: [:edit, :update, :destroy]
 
     def show
     end
@@ -15,16 +17,16 @@ before_action:set_article, only: [:show,:edit,:update,:destroy]
           
     end
 
-        def create
-            @article = Article.new(articles_prams)
-            @article.user = User.first
-            if @article.save
-                flash[:notice] = "Article was created successfully."
-              redirect_to @article 
-            else
-              render 'new'
-            end
-          end
+   def create
+    @article = Article.new(articles_prams)
+    @article.user = current_user
+      if @article.save
+        flash[:notice] = "Article was created successfully."
+         redirect_to @article 
+      else
+        render 'new'
+      end
+    end
 
 
          
@@ -51,6 +53,14 @@ before_action:set_article, only: [:show,:edit,:update,:destroy]
 
         def articles_prams
           params.require(:article).permit(:title, :description)
+        end
+
+
+        def require_same_user
+          if current_user != @article.user && !current_user.admin?
+            flash[:alert] = "You can only edit or delete your own article"
+            redirect_to @article
+          end
         end
       
          end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,12 @@
 class UsersController <ApplicationController
+
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :require_user, only: [:edit, :update]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
    def new
       @user = User.new
    end
    def show
-    @user = User.find(params[:id])
     @article = @user.articles
   end
 
@@ -22,11 +25,11 @@ class UsersController <ApplicationController
     end
 
     def edit
-      @user = User.find(params[:id])
+
     end
   
     def update
-      @user = User.find(params[:id])
+     
       if @user.update(user_params)
         flash[:notice] = "Your account information was successfully updated"
         redirect_to @user
@@ -34,12 +37,29 @@ class UsersController <ApplicationController
         render 'edit'
       end
     end
+
+    def destroy
+      @user.destroy
+      session[:user_id] = nil  if @user == current_user
+      flash[:notice] = "Account and all associated articles successfully deleted"
+      redirect_to articles_path
+    end
   
     private
     def user_params
       params.require(:user).permit(:username, :email, :password)
     end
 
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    def require_same_user
+      if current_user != @user && !current_user.admin?
+        flash[:alert] = "You can only edit your own account"
+        redirect_to @user
+      end
+    end
    
   
     
